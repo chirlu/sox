a53682a6e88750c10684b905d5e6570a4c5afe32
robs <robs>
2008-11-16 09:23:44 +0000

    updates

--- scripts/synth.sh
+++ scripts/synth.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# SoX script: synth.sh               (c) 2008 robs@users.sourceforge.net
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 sox=../src/sox
 play=../src/play
 
@@ -11,9 +27,19 @@ B1="pl %-15 pl %-10 pl %-7 pl %-3 pl %2 pl %6"
 e="delay 0 .02 .04 .06 .08 .1 remix - overdrive 40 gain -10 fade 0"
 s="$sox -q -n -p synth 0 0 0 80 87"
 
-$play -m "|$sox -c 2 -n -p synth tpdf fade 0 .4 .4 pad .1 gain -9 highpass 1k reverb 20 repeat 72" \
+$play -m \
 "|$sox \
-\"|$sox -n -p trim 0 1.5\" \
+\"|$sox -n -p synth noise fade 0 .5 .48 trim 0 .15\" \
+\"|$sox -n -p synth noise fade h 0 .26 .11 gain -35 lowpass -1 12k\" \
+-p splice .15,.06,0 gain -14 lowpass -1 12k highpass -1 9k \
+equalizer 14k 1.3 13 \
+equalizer 9500 10 8 \
+equalizer 7000 10 8 \
+equalizer 5200 10 8 \
+equalizer 3800 10 8 \
+equalizer 1500 10 8 pad 0 .21 remix 1 1 repeat 72" \
+"|$sox \
+\"|$sox -n -p trim 0 1.4\" \
 \"|$s $A0 $e 4 .1 bend .4,+200,.3\" \
 \"|$s $A0 $e 4 .1 bend .4,+200,.3\" \
 \"|$s $B0 $e 4 .1 bend .4,-200,.3\" \
