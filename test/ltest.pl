6ada3918c43f59d76d2e0a5e29c2564c15778a0a
robs <robs>
2008-12-30 09:16:30 +0000

    no need for ding; sox can do it

--- test/ltest.pl
+++ test/ltest.pl
@@ -34,82 +34,82 @@ my $effect='resample';
 #my ($rate0,$rate1)=(44100,44100); # sample rates
 my ($rate0,$rate1)=(8000,22050); # sample rates
 my $p=400;  # silence before/after tonepulse
-my $env="4000:16000:4000"; # attack, duration, drop
+my $attack=4000;
+my $duration=16000;
 
 #my ($rate0,$rate1)=(22050,8000); # sample rates
 #my $p=1102;  # silence before/after tonepulse
-#my $env="-e11025:44100:11025"; # attack, duration, drop
+#my $attack=11025;
+#my $duration=44100;
 
 # parse commandline arguments
 my $updown = 0; # set to 1 for up/down rate-conversion test
-my ($ding,$model,$t,$rms,$lim)=("./ding","./model","sw",11585.2, 0.5);
+my ($model,$t,$rms,$lim)=("./model","sw",11585.2, 0.5);
 while ($_ = shift @ARGV) {
-	if ($_ eq "-l") {
-		($ding,$model,$t,$rms,$lim)=("./lding","./lmodel","sl",759250125.0, 50.0);
-	} elsif ($_ eq "-ud") {
-		$updown=1;
-	} else {
-		unshift @ARGV,$_;
-		last;
-	}
+  if ($_ eq "-l") {
+    ($model,$t,$rms,$lim)=("./lmodel","sl",759250125.0, 50.0);
+  } elsif ($_ eq "-ud") {
+    $updown=1;
+  } else {
+    unshift @ARGV,$_;
+    last;
+  }
 }
 if ($#ARGV >= 0) {
-	$effect="@ARGV";
+  $effect="@ARGV";
 }
 
 my $ratechange=0;
-if ($effect =~ m{^(resample|polyphase|rate)}) {
-	$ratechange=1;
+if ($effect =~ m{^(rabbit|resample|polyphase|rate)}) {
+  $ratechange=1;
 }
 
 # output a nice header explaining the data 
 if ($ratechange==0) {
-	print("# Testing $sox -r$rate0 d/i0.xx.$t d/j0.xx.$t $effect\n");
+  print("# Testing $sox -c1 -r$rate0 i0.xx.$t j0.xx.$t $effect\n");
 } else {
-	print("# Testing $sox -r$rate0 d/i*.$t -r$rate1 d/u*.$t $effect\n");
-	if ($updown==1) {
-		print("#   then back down to $rate0\n");
-	}
+  print("# Testing $sox -c1 -r$rate0 i*.$t -r$rate1 u*.$t $effect\n");
+  if ($updown==1) {
+    print("#   then back down to $rate0\n");
+  }
 }
-print("#   with tone pulses from 0.00 to 0.99 percent of Nyquist\n");
-print("#   produced by $ding -f0.xx -v0.5 -e$p:$env:$p d/i0.xx.$t\n");
+print("#   with tone pulses from 0.01 to 0.99 percent of Nyquist\n");
 
 # generate the test data
-mkdir("d",0775);
 my $f;
 my %q;
 my $nyq = ($rate0<=$rate1)? 1.0:($rate1/$rate0); 
-for ($f=0.00; $f<1.001; $f+=0.01) {
-	my @mod;
+my $l=$attack + $duration + $attack;
+my $env="$attack:$duration:$attack"; # attack, duration, drop
+for ($f=0.01; $f<.999; $f+=0.01) {
+  my @mod;
 
-	#if ($f>0.995) { $f=0.999; }
-	my $s=sprintf("%4.2f",$f);
-	#print "$ding -f$s -v0.5 -d1.0 -e$p:$env:$p d/i$s.$t\n";
-	qx{$ding -f$s -v0.5 -d1.0 -e$p:$env:$p d/i$s.$t &>/dev/null};
-	if ($ratechange==0) {
-		qx{$sox -r$rate0 d/i$s.$t -r$rate0 d/j$s.$t $effect} ;
-		@mod = grep {/v2max/} qx{$model -f$s -e$env $rate0 d/j$s.$t 2>&1};
-	} else {
-		qx{$sox -r$rate0 d/i$s.$t -r$rate1 d/u$s.$t $effect 2>/dev/null};
-		if ($updown) {
-			qx{$sox -r$rate1 d/u$s.$t -r$rate0 d/o$s.$t $effect 2>/dev/null};
-			@mod = grep {/v2max/} qx{$model -f$s -e$env $rate0:$rate0 d/o$s.$t 2>&1};
-		}else{
-			@mod = grep {/v2max/} qx{$model -f$s -e$env $rate0:$rate1 d/u$s.$t 2>&1};
-		}
-	}
-	print STDERR "$s: ",@mod;
-	$_=shift(@mod);
-	if (m{s2max *([0-9.]+), *v2max *([0-9.]+), *rmserr *(-?[0-9.]+)}) {
-		#print("$s $1\n");
-		#print("$s $1 $3\n");
-		my $v = ($1 > $lim)? $1 : $lim;
-		my $r = ($3 > $lim)? $3 : $lim;
-		my $dbv = 20*log($v/$rms)/log(10);
-		my $dbr = 20*log($r/$rms)/log(10);
-		printf("%s %.3f %.3f\n",$s,$dbv,$dbr);
-	}
-	#unlink "d/i$s.$t","d/u$s.$t","d/o$s.$t";
+  my $s=sprintf("%4.2f",$f);
+  qx{$sox -2r2 -n i$s.$t synth ${l}s sin $s vol .5 fade h ${attack}s ${l}s ${attack}s pad ${p}s ${p}s};
+  if ($ratechange==0) {
+    qx{$sox -c1 -r$rate0 i$s.$t -r$rate0 o$s.$t $effect} ;
+    @mod = grep {/v2max/} qx{$model -f$s -e$env $rate0 o$s.$t 2>&1};
+  } else {
+    qx{$sox -c1 -r$rate0 i$s.$t -r$rate1 u$s.$t $effect 2>/dev/null};
+    if ($updown) {
+      qx{$sox -c1 -r$rate1 u$s.$t -r$rate0 o$s.$t $effect 2>/dev/null};
+      @mod = grep {/v2max/} qx{$model -f$s -e$env $rate0:$rate0 o$s.$t 2>&1};
+    }else{
+      @mod = grep {/v2max/} qx{$model -f$s -e$env $rate0:$rate1 u$s.$t 2>&1};
+    }
+  }
+  print STDERR "$s: ",@mod;
+  $_=shift(@mod);
+  if (m{s2max *([0-9.]+), *v2max *([0-9.]+), *rmserr *(-?[0-9.]+)}) {
+    #print("$s $1\n");
+    #print("$s $1 $3\n");
+    my $v = ($1 > $lim)? $1 : $lim;
+    my $r = ($3 > $lim)? $3 : $lim;
+    my $dbv = 20*log($v/$rms)/log(10);
+    my $dbr = 20*log($r/$rms)/log(10);
+    printf("%s %.3f %.3f\n",$s,$dbv,$dbr);
+  }
+  unlink "i$s.$t","u$s.$t","o$s.$t";
 }
 
 exit 0;
--- test/wtest.pl
+++ test/wtest.pl
@@ -28,7 +28,8 @@ my $fmt='';
 
 my $rate=8000; # sample rates
 my $p=200;  # silence before/after tonepulse
-my $env="400:800:400"; # attack, duration, drop
+my $attack=400;
+my $duration=800;
 
 my ($ding,$model,$t,$rms,$lim)=("./ding","./model","sw",11585.2, 0.5);
 
@@ -36,69 +37,68 @@ my $effect=''; # you may want to try a filter
 
 # parse commandline arguments
 while ($_ = shift @ARGV) {
-	if (m{^-[tgai]$}) {
-		$fmt=$_;
-	} else {
-		unshift @ARGV,$_;
-		last;
-	}
+  if (m{^-[tgai]$}) {
+    $fmt=$_;
+  } else {
+    unshift @ARGV,$_;
+    last;
+  }
 }
 if ($#ARGV >= 0) {
-	$effect="@ARGV";
+  $effect="@ARGV";
 }
+
+my $env="$attack:$duration:$attack"; # attack, duration, drop
 
 # output a nice header explaining the data 
 print("# Testing gsm compress/decompress ");
 if ($fmt eq '-t') {
-	print("by $toast\n");
+  print("by $toast\n");
 }else{
-	print("by $sox -r$rate in.sw $fmt out.wav $effect\n");
+  print("by $sox -r$rate in.sw $fmt out.wav $effect\n");
 }
-print("#   with tone pulses from 0.00 to 0.99 percent of Nyquist\n");
-print("#   produced by $ding -f0.xx -v0.5 -e$p:$env:$p d/i0.xx.$t\n");
+print("#   with tone pulses from 0.01 to 0.99 percent of Nyquist\n");
 print("#   col 1 is frequency/Nyquist\n");
 print("#   col 2 is (power) dB gain\n");
 print("#   col 3 is (power) dB level of error signal\n");
 print("#\n#freq dB-gain dB-error\n");
 
 # generate the test data
-mkdir("d",0775);
 my $f;
 my %q;
 my $nyq = 1.0; 
-for ($f=0.00; $f<1.001; $f+=0.01) {
-	my @mod;
+my $l=$attack + $duration + $attack;
+for ($f=0.01; $f<0.999; $f+=0.01) {
+  my @mod;
 
-	#if ($f>0.995) { $f=0.999; }
-	my $s=sprintf("%4.2f",$f);
-	#print "$ding -f$s -v0.5 -e$p:$env:$p -d1.0 d/i$s.$t\n";
-	qx{$ding -f$s -v0.5 -e$p:$env:$p -d1.0 d/i$s.$t &>/dev/null};
+  my $s=sprintf("%4.2f",$f);
+  qx{$sox -2r2 -n i$s.$t synth ${l}s sin $s vol .5 fade h ${attack}s ${l}s ${attack}s pad ${p}s ${p}s};
 
-	if ($fmt eq '-t') {
-		qx{$toast -l d/i$s.$t};
-		qx{$toast -dl d/i$s.$t.gsm};
-	}else{
-		qx{cp d/i$s.$t d/a$s.$t 2>/dev/null};
-		qx{$sox -r$rate d/i$s.$t -g d/g$s.wav $effect 2>/dev/null};
-		unlink "d/i$s.$t";
-		qx{$sox d/g$s.wav d/i$s.$t 2>/dev/null};
-		qx{cp d/i$s.$t d/b$s.$t 2>/dev/null};
-		unlink "d/g$s.wav";
-	}
+  if ($fmt eq '-t') {
+    qx{$toast -l i$s.$t};
+    qx{$toast -dl i$s.$t.gsm};
+  }else{
+    qx{cp i$s.$t a$s.$t 2>/dev/null};
+    qx{$sox -r$rate i$s.$t -g g$s.wav $effect 2>/dev/null};
+    unlink "i$s.$t";
+    qx{$sox g$s.wav i$s.$t 2>/dev/null};
+    qx{cp i$s.$t b$s.$t 2>/dev/null};
+    unlink "g$s.wav";
+  }
 
-	@mod = grep {/v2max/} qx{$model -f$s -e$env $rate d/i$s.$t 2>&1};
-	print STDERR "$s: ",@mod;
-	$_=shift(@mod);
-	if (m{s2max *([0-9.]+), *v2max *([0-9.]+), *rmserr *(-?[0-9.]+)}) {
-		#print("$s $1\n");
-		#print("$s $1 $3\n");
-		my $v = ($1 > $lim)? $1 : $lim;
-		my $r = ($3 > $lim)? $3 : $lim;
-		my $dbv = 20*log($v/$rms)/log(10);
-		my $dbr = 20*log($r/$rms)/log(10);
-		printf("%s %.3f %.3f\n",$s,$dbv,$dbr);
-	}
-	unlink "d/i$s.$t";
+  @mod = grep {/v2max/} qx{$model -f$s -e$env $rate i$s.$t 2>&1};
+  print STDERR "$s: ",@mod;
+  $_=shift(@mod);
+  if (m{s2max *([0-9.]+), *v2max *([0-9.]+), *rmserr *(-?[0-9.]+)}) {
+    #print("$s $1\n");
+    #print("$s $1 $3\n");
+    my $v = ($1 > $lim)? $1 : $lim;
+    my $r = ($3 > $lim)? $3 : $lim;
+    my $dbv = 20*log($v/$rms)/log(10);
+    my $dbr = 20*log($r/$rms)/log(10);
+    printf("%s %.3f %.3f\n",$s,$dbv,$dbr);
+  }
+  unlink "i$s.$t";
 }
 print("#freq dB-gain dB-error\n");
 exit 0;
