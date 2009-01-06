36cc2a77dc286bd596f59166f6737ffb4a5e2e85
robs <robs>
2009-01-06 00:00:49 +0000

    fix [2487589] Glitch dither -s

--- src/dither_fir.h
+++ src/dither_fir.h
@@ -4,7 +4,6 @@ static int NAME(sox_effect_t * effp, const sox_sample_t * ibuf,
 {
   priv_t * p = (priv_t *)effp->priv;
   size_t len = *isamp = *osamp = min(*isamp, *osamp);
-  int dummy = 0;
 
   while (len--) {
     double r = p->am0 * RANQD1 + p->am1 * RANQD1;
@@ -12,7 +11,9 @@ static int NAME(sox_effect_t * effp, const sox_sample_t * ibuf,
     int j = 0;
     CONVOLVE
     assert(j == N);
-    *obuf = SOX_ROUND_CLIP_COUNT(d + r, dummy);
+    *obuf = d + r < 0? d + r <= SOX_SAMPLE_MIN - .5? SOX_SAMPLE_MIN: d + r - .5 
+          : d + r >= SOX_SAMPLE_MAX - (1 << (31-PREC)) + 0.5?
+          SOX_SAMPLE_MAX - (1 << (31-PREC)): d + r + .5;
     error = ((*obuf++ + (1 << (31-PREC))) & (-1 << (32-PREC))) - d;
     p->pos = p->pos? p->pos - 1 : p->pos - 1 + N;
     p->previous_errors[p->pos + N] = p->previous_errors[p->pos] = error;
--- src/dither_iir.h
+++ src/dither_iir.h
@@ -5,7 +5,6 @@ static int NAME(sox_effect_t * effp, const sox_sample_t * ibuf,
 {
   priv_t * p = (priv_t *)effp->priv;
   size_t len = *isamp = *osamp = min(*isamp, *osamp);
-  int dummy = 0;
 
   while (len--) {
     double r = p->am0 * RANQD1 + p->am1 * RANQD1;
@@ -14,7 +13,9 @@ static int NAME(sox_effect_t * effp, const sox_sample_t * ibuf,
     CONVOLVE
     assert(j == N);
     d = *ibuf++ - output;
-    *obuf = SOX_ROUND_CLIP_COUNT(d + r, dummy);
+    *obuf = d + r < 0? d + r <= SOX_SAMPLE_MIN - .5? SOX_SAMPLE_MIN: d + r - .5 
+          : d + r >= SOX_SAMPLE_MAX - (1 << (31-PREC)) + 0.5?
+          SOX_SAMPLE_MAX - (1 << (31-PREC)): d + r + .5;
     error = ((*obuf++ + (1 << (31-PREC))) & (-1 << (32-PREC))) - d;
     p->pos = p->pos? p->pos - 1 : p->pos - 1 + N;
     p->previous_errors[p->pos + N] = p->previous_errors[p->pos] = error;
