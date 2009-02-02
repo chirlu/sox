6beda2fd2ef14f2165b4cf33a48fd9a7fdd9c84d
rrt <rrt>
2009-02-02 21:20:17 +0000

    Fix old call to SOX_SAMPLE_TO_FLOAT_32BIT.

--- src/ladspa.c
+++ src/ladspa.c
@@ -237,7 +237,7 @@ static int sox_ladspa_flow(sox_effect_t * effp, const sox_sample_t *ibuf, sox_sa
     if (l_st->input_port != ULONG_MAX) {
       /* Copy the input; FIXME: Assume LADSPA_Data == float! */
       for (i = 0; i < len; i++)
-        buf[i] = SOX_SAMPLE_TO_FLOAT_32BIT(ibuf[i]);
+        buf[i] = SOX_SAMPLE_TO_FLOAT_32BIT(ibuf[i], effp->clips);
 
       /* Connect the input port */
       l_st->desc->connect_port(l_st->handle, l_st->input_port, buf);
