ef624ab558952482058e8fd4444afc1a0a1cdba6
Chris Bagwell <chris@cnpbagwell.com>
2012-03-04 11:06:47 -0600

    sox_format_quit() should reset format counts
    If an app is calling sox_format_init() and sox_format_quit()
    multiple times then count of formats was not correct and
    worse there were pointers to unloaded symbols sticking around.

--- src/formats.c
+++ src/formats.c
@@ -1239,6 +1239,8 @@ void sox_format_quit(void) /* Cleanup things.  */
   int ret;
   if (plugins_initted && (ret = lt_dlexit()) != 0)
     lsx_fail("lt_dlexit failed with %d error(s): %s", ret, lt_dlerror());
+  plugins_initted = sox_false;
+  nformats = NSTATIC_FORMATS;
 #endif
 }
 
