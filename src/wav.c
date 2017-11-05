6e177c455fb554327ff8125b6e6dde1568610abe
Mans Rullgard <mans@mansr.com>
2017-11-05 16:29:28 +0000

    wav: fix crash if channel count is zero (CVE-2017-11332)

--- src/wav.c
+++ src/wav.c
@@ -712,6 +712,11 @@ static int startread(sox_format_t * ft)
     else
         lsx_report("User options overriding channels read in .wav header");
 
+    if (ft->signal.channels == 0) {
+        lsx_fail_errno(ft, SOX_EHDR, "Channel count is zero");
+        return SOX_EOF;
+    }
+
     if (ft->signal.rate == 0 || ft->signal.rate == dwSamplesPerSecond)
         ft->signal.rate = dwSamplesPerSecond;
     else
