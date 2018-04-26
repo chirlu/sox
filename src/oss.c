342d3e71fc197e1dfb45f7dd84ee734ad0a8f79d
Mans Rullgard <mans@mansr.com>
2018-04-26 19:31:10 +0100

    oss: fix input buffer overrun [bug #300]
    Each iteration of the write loop should only read the remaining number
    of samples, not the full input buffer size.

--- src/oss.c
+++ src/oss.c
@@ -369,7 +369,7 @@ static size_t osswrite(
         size_t cbStride;
         int cbWritten;
 
-        cStride = cInput;
+        cStride = cInputRemaining;
         if (cStride > pPriv->cOutput) {
             cStride = pPriv->cOutput;
         }
