784021f41f97a65471d961d39474c85ba00318de
Mans Rullgard <mans@mansr.com>
2020-08-27 13:04:25 +0100

    sunaudio: fix input buffer overrun
    The write loop should use the remaining input size, not the initial
    value.

--- src/sunaudio.c
+++ src/sunaudio.c
@@ -443,7 +443,7 @@ static size_t sunwrite(
         size_t cbStride;
         int cbWritten;
 
-        cStride = cInput;
+        cStride = cInputRemaining;
         if (cStride > pPriv->cOutput) {
             cStride = pPriv->cOutput;
         }
